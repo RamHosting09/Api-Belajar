ccaf50b3993ff0ccaee5d3a66a78fe4e6ba9443c
RamHosting09 <rajamanalu2011@gmail.com>
2025-04-07 11:52:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
-# Data dummy
 data_api = {
     "nama": "RamHosting09",
     "alamat": "Indonesia",
     "kontak": "1234567890"
 }
-# Endpoint API
 @app.route('/api/info', methods=['GET'])
 def info():
     return jsonify(data_api)
